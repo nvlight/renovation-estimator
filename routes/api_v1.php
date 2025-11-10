cf451f2e7edcb1c354301c92a1db589c60d42c81
nvlight <lightstyle11@mail.ru>
2025-11-10 13:59:15 +0300

    test push 2

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\V1\RoomController;
 use App\Http\Controllers\Api\V1\RoomWallController;
 use Illuminate\Support\Facades\Route;
 
-// test string for push
+// test string for push - v2
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('v1/project', ProjectController::class);
     Route::apiResource('v1/project/{project}/rooms', RoomController::class);
