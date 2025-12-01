8001e61fbb87b2ef53ca36a3bea7b9d64267eca3
nvlight <lightstyle11@mail.ru>
2025-12-01 15:45:35 +0300

    improved materials view page. add standalone component for images. uploading images work with standard q-loader component

---
+++ quasar_frontend/src/pages/MaterialImage.vue
@@ -0,0 +1,196 @@
+<template>
+  <div>
+    <h5>Картинки </h5>
+
+    <div v-if="sortedImages"
+         class="flex q-mt-md"
+         style="gap: 10px;"
+    >
+      <div v-for="(item,i) in sortedImages" :key="i">
+        <img
+          :src="item.url"
+          width="200px"
+          height="100px"
+          alt=""
+        />
+        <div class="flex justify-center">
+          <q-btn
+            flat
+            round
+            size="md"
+            color="positive"
+            icon="chevron_left"
+            @click="moveMaterialImage(item.id,'left')"
+          />
+          <q-btn
+            flat
+            round
+            size="md"
+            color="positive"
+            icon="chevron_right"
+            @click="moveMaterialImage(item.id,'right')"
+          />
+
+          <q-btn
+            flat
+            round
+            size="md"
+            color="negative"
+            icon="delete"
+            @click="deleteMaterialImage(item.id)"
+          />
+        </div>
+      </div>
+    </div>
+
+    <!-- upload images -->
+    <div class="q-ma-md ">
+      <q-uploader
+        ref="uploaderRef"
+        :url="uploaderUrl"
+        :headers="uploadHeaders"
+        max-files="1"
+        accept="image/*"
+        field-name="name"
+        :form-fields="formFields"
+        :auto-upload="false"
+        :max-file-size="20000000"
+        @added="onFileAdded"
+        @uploaded="onFileUploaded"
+        label="Выберите изображение (макс. 1200x800 px)"
+        square
+        flat
+        bordered
+        style="max-width: 300px"
+      >
+      </q-uploader>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {computed, onMounted, ref, watch} from "vue";
+import {Notify} from "quasar";
+import {api} from "@/boot/axios.js";
+
+const props = defineProps({
+  materialId: {
+    type: Number,
+    required: true,
+  },
+  images: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const localImages = ref([]);
+
+// Код ниже отвечает за сохранения картинки
+// Реактивные данные
+const uploaderRef = ref(null)
+
+const sortedImages = computed(() => {
+  if (!localImages.value || !Array.isArray(localImages.value)) {
+    return [];
+  }
+  return [...localImages.value].sort((a, b) => a.sort - b.sort);
+});
+
+const deleteMaterialImage = async (material_image_id) => {
+  if (!confirm('Действительно удалить?')) return;
+
+  const del = await api.delete(`/v1/material_image/${material_image_id}`);
+  if (del?.status === 200) {
+
+    localImages.value = localImages.value.filter(item => item.id !== material_image_id);
+
+    Notify.create({
+      type: 'positive',
+      message: 'Картинка удалена!',
+    });
+  } else {
+    Notify.create({
+      type: 'negative',
+      message: 'Ошибка при удалении!',
+    });
+  }
+}
+
+const moveMaterialImage = async (material_image_id, direction) => {
+  const api_dir = direction === 'left' ? 'left' : 'right';
+  const move = await api.patch(`/v1/material_image/${material_image_id}/to_${api_dir}`);
+  if (move?.status === 200 && move.data.success) {
+
+    localImages.value = move.data.images;
+  }
+}
+
+const uploaderUrl = computed(() => {
+  return `${api.defaults.baseURL}/v1/material_image/`;
+});
+const uploadHeaders = [
+  { name: 'Authorization', value: `Bearer ${localStorage.getItem('auth_token')}` },
+  { name: 'Accept', value: 'application/json' }
+];
+const formFields = computed(() => [
+  { name: 'material_id', value: props.materialId }
+]);
+
+const checkImageDimensions = (file) => {
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const img = new Image()
+    img.onload = () => {
+      if (img.naturalWidth > 1200 || img.naturalHeight > 800) {
+        Notify.create({
+          type: 'negative',
+          message: 'Изображение слишком большое (макс. 1200x800 px)'
+        })
+
+        uploaderRef.value.removeFile(file)
+      }
+    }
+    img.src = e.target.result
+  }
+  reader.readAsDataURL(file)
+}
+
+const onFileUploaded = (info) => {
+  if (info.xhr.status === 201) {
+    const result = JSON.parse(info.xhr.response);
+    if (result.success){
+      localImages.value = result.images;
+      Notify.create({
+        type: 'positive',
+        message: 'Файл успешно загружен'
+      })
+    }
+  }
+}
+
+const onFileAdded = (files) => {
+  if (files.length === 0) return
+
+  const file = files[0];
+
+  checkImageDimensions(file)
+}
+
+onMounted(() => {
+});
+
+// Каждый раз при изменении props.images
+watch(
+  () => props.images,
+  (newVal) => {
+    localImages.value = [...newVal]
+  },
+  {immediate: true} // важное! заполнит localImages сразу
+);
+
+</script>
+
+<style scoped>
+</style>
--- quasar_frontend/src/pages/MaterialView.vue
+++ quasar_frontend/src/pages/MaterialView.vue
@@ -5,7 +5,7 @@
       <q-breadcrumbs-el :label="`Проект ${materialId}`" />
     </q-breadcrumbs>
 
-    <h5>Просмотр материала ({{ materialId }})</h5>
+    <h5>{{ materialTitle }} (id={{ materialId }})</h5>
 
     <q-table
       title=""
@@ -16,74 +16,11 @@
     >
     </q-table>
 
-    <div v-if="sortedImages"
-         class="flex q-mt-md"
-         style="gap: 10px;"
-    >
-      <div v-for="(item,i) in sortedImages" :key="i">
-        <img
-          :src="item.url"
-          width="200px"
-          height="100px"
-          alt=""
-        />
-        <div class="flex justify-center">
-          <q-btn
-            flat
-            round
-            size="md"
-            color="positive"
-            icon="chevron_left"
-            @click="moveMaterialImage(item.id,'left')"
-          />
-          <q-btn
-            flat
-            round
-            size="md"
-            color="positive"
-            icon="chevron_right"
-            @click="moveMaterialImage(item.id,'right')"
-          />
+    <material-image
+      :materialId="materialId"
+      :images="material?.images"
+    />
 
-          <q-btn
-            flat
-            round
-            size="md"
-            color="negative"
-            icon="delete"
-            @click="deleteMaterialImage(item.id)"
-          />
-        </div>
-      </div>
-    </div>
-
-    <!--    <div><pre>{{ material?.images }}</pre></div>-->
-
-    <!-- upload images -->
-    <div class="q-ma-md ">
-      <q-uploader
-        ref="uploaderRef"
-        max-files="1"
-        accept="image/*"
-        :auto-upload="false"
-        :max-file-size="20000000"
-        @added="checkImageDimensions"
-        @removed="onFileRemoved"
-        label="Выберите изображение (макс. 1200x800 px)"
-        square
-        flat
-        bordered
-        style="max-width: 300px"
-      />
-
-      <q-btn
-        label="Отправить на сервер"
-        @click="submitFile"
-        color="primary"
-        class="q-mt-md"
-        :disable="!selectedFile"
-      />
-    </div>
   </q-page>
 </template>
 
@@ -91,8 +28,7 @@
 import {useRoute} from 'vue-router';
 import {computed, onMounted, ref} from "vue";
 import {useMaterialsStore} from "@/stores/materials.js";
-import {api} from "@/boot/axios.js";
-import {Notify} from "quasar";
+import MaterialImage from "@/pages/MaterialImage.vue";
 
 const materialStore = useMaterialsStore();
 const route = useRoute();
@@ -120,7 +56,7 @@ const columns = [
 
 const rows = ref([]);
 
-const materialId = route.params.material_id;
+const materialId = Number(route.params.material_id);
 const material = ref({});
 
 const loadMaterial = (materialId) => {
@@ -140,121 +76,13 @@ const loadMaterial = (materialId) => {
   });
 }
 
-const sortedImages = computed(() => {
-  if (!material.value.images || !Array.isArray(material.value.images)) {
-    return [];
-  }
-  return [...material.value.images].sort((a, b) => a.sort - b.sort);
-});
-
-const deleteMaterialImage = async (material_image_id) => {
-  if (!confirm('Действительно удалить?')) return;
-
-  const del = await api.delete(`/v1/material_image/${material_image_id}`);
-  if (del?.status === 200) {
-
-    material.value.images = material.value.images.filter(item => item.id !== material_image_id);
-
-    Notify.create({
-      type: 'positive',
-      message: 'Картинка удалена!',
-    });
-  } else {
-    Notify.create({
-      type: 'negative',
-      message: 'Ошибка при удалении!',
-    });
-  }
-}
-
-const moveMaterialImage = async (material_image_id, direction) => {
-  const api_dir = direction === 'left' ? 'left' : 'right';
-  const move = await api.patch(`/v1/material_image/${material_image_id}/to_${api_dir}`);
-  if (move?.status === 200 && move.data.success) {
-    material.value.images = move.data.images;
-  }
-}
+const materialTitle = computed(() => {
+  return rows.value.filter(i => i.key === 'title')[0]?.value;
+})
 
 onMounted(() => {
   loadMaterial(materialId);
 });
-
-
-// Код ниже отвечает за сохранения картинки
-// Реактивные данные
-const uploaderRef = ref(null)
-const selectedFile = ref(null)
-
-// Методы
-const checkImageDimensions = (files) => {
-  const file = files[0]
-  const img = new Image()
-
-  img.onload = () => {
-    if (img.width > 1200 || img.height > 800) {
-      Notify.create({
-        type: 'negative',
-        message: 'Размер изображения не должен превышать 1200x800 px'
-      })
-      uploaderRef.value.removeFile(file)
-    } else {
-      selectedFile.value = file
-    }
-    URL.revokeObjectURL(img.src)
-  }
-
-  img.onerror = () => {
-    Notify.create({
-      type: 'negative',
-      message: 'Файл не является изображением'
-    })
-    uploaderRef.value.removeFile(file)
-    URL.revokeObjectURL(img.src)
-  }
-
-  img.src = URL.createObjectURL(file)
-}
-
-const onFileRemoved = () => {
-  selectedFile.value = null
-}
-
-const submitFile = async () => {
-  if (!selectedFile.value) {
-    Notify.create({
-      type: 'warning',
-      message: 'Сначала выберите файл'
-    })
-    return
-  }
-
-  try {
-    const formData = new FormData()
-    formData.append('name', selectedFile.value);
-    formData.append('material_id', materialId);
-
-    const result = await api.post('/v1/material_image/', formData);
-    if (result.status === 201){
-      material.value.images = result.data.images;
-    }
-
-    Notify.create({
-      type: 'positive',
-      message: 'Файл успешно загружен'
-    })
-
-    // Очищаем после успешной загрузки
-    uploaderRef.value.removeQueuedFiles()
-    selectedFile.value = null
-
-  } catch (error) {
-    Notify.create({
-      type: 'negative',
-      message: 'Ошибка загрузки файла'
-    })
-    console.error('Upload error:', error)
-  }
-}
 
 </script>
 
